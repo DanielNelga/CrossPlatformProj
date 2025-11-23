866be0c7da09882873bfe96544e36393ce891272
Daniel Nelga <g00464658@atu.ie>
2025-11-23 18:23:22 +0000

    fixed issue of movie list not appearing on screen from jsonFile

--- CrossPlatformProject/movie.cs
+++ CrossPlatformProject/movie.cs
@@ -5,8 +5,11 @@ namespace CrossPlatformProject
     public partial class Movie
     {
         //Means to search and capture data from the json file
+        [JsonPropertyName("title")]
         public string Title { get; set; }
+        [JsonPropertyName("genre")]
         public List<string> Genre { get; set; }
+        [JsonPropertyName("emoji")]
         public string Emoji { get; set; }
     }
 }
